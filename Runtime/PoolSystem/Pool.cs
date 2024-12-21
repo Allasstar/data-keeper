6b3a509957d1b82b0fb0504e88e689e522cf8bc4
Roman Shulevka <micrarriors@gmail.com>
2024-12-21 13:12:49 +0200

    Pool System

---
+++ Runtime/PoolSystem/Pool.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using DataKeeper.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace DataKeeper.PoolSystem
+{
+    [Serializable]
+    public class Pool<T> where T : Component
+    {
+        [SerializeField] private T _poolPrefab;
+        [SerializeField] private Optional<int> _prewarm = new Optional<int>();
+        [SerializeField] private Optional<int> _maxActive = new Optional<int>();
+        
+        private Transform _poolContainer;
+        private List<T> _poolInactive;
+        private List<T> _poolActive;
+
+        public int GetPoolPrefabID() => _poolPrefab.GetHashCode();
+        public string GetPoolPrefabName() => _poolPrefab.name;
+
+       public virtual void Initialize()
+       {
+           new PoolContainer<T>(GetPoolPrefabID(), GetPoolPrefabName())
+               .Deconstruct(out _poolContainer, out _poolInactive, out _poolActive);
+
+           if (!_prewarm.Enabled) return;
+           
+           if (_poolInactive.Count < _prewarm.Value)
+           {
+               for (int i = 0; i < _prewarm.Value - _poolInactive.Count; i++)
+               {
+                   Create();
+               }
+           }
+       }
+       
+       public virtual T Create()
+       {
+           var obj = Object.Instantiate(_poolPrefab, _poolContainer);
+           obj.gameObject.name = $"{_poolPrefab.name} [{obj.GetInstanceID()}]";
+           obj.gameObject.SetActive(false);
+           _poolInactive.Add(obj);
+           return obj;
+       }
+
+       public virtual T Get()
+       {
+           T poolObject;
+
+           if (_maxActive is { Enabled: true, Value: > 0 } && _poolActive.Count >= _maxActive.Value)
+           {
+               Release(_poolActive[0]);
+           }
+           
+           if (_poolInactive.Count > 0)
+           {
+               poolObject = _poolInactive[0];
+           }
+           else
+           {
+               poolObject = Create();
+           }
+           
+           _poolInactive.Remove(poolObject);
+           _poolActive.Add(poolObject);
+           poolObject.gameObject.SetActive(true);
+           return poolObject;
+       }
+       
+       public T Get(out Action releaseAction)
+       {
+           var obj = Get();
+           releaseAction = () => Release(obj);
+           return obj;
+       }
+
+       public virtual void Release(T poolObject)
+       {
+           poolObject.gameObject.SetActive(false);
+           poolObject.transform.SetParent(_poolContainer);
+           
+           _poolActive.Remove(poolObject);
+           
+           if(_poolInactive.Contains(poolObject)) return;
+           _poolInactive.Add(poolObject);
+       }
+
+       public virtual void ReleaseAll()
+       {
+           foreach (var component in _poolActive.ToArray())
+           {
+               Release(component);
+           }
+           
+           _poolActive.Clear();
+       }
+    }
+}
---
+++ Runtime/PoolSystem/PoolContainer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DataKeeper.PoolSystem
+{
+    public class PoolContainer<T>
+    {
+        private static Dictionary<int, Transform> PoolParentDictionary = new Dictionary<int, Transform>();
+        private static Dictionary<int, List<T>> PoolInactiveDictionary = new Dictionary<int, List<T>>();
+
+        private int _poolPrefabID;
+        private string _poolPrefabName;
+        
+        private Transform GetPoolParent()
+        {
+            Transform poolParent;
+          
+            if (PoolParentDictionary.ContainsKey(_poolPrefabID))
+            {
+                poolParent = PoolParentDictionary[_poolPrefabID];
+            }
+            else
+            {
+                poolParent = new GameObject().transform;
+                Object.DontDestroyOnLoad(poolParent);
+                poolParent.name = $":: [{typeof(T).Name}] > {_poolPrefabName}";
+                PoolParentDictionary[_poolPrefabID] = poolParent;
+            }
+            
+            return poolParent;
+        }
+
+        private List<T> GetPoolInactive()
+        {
+            List<T> poolInactive;
+            if (PoolInactiveDictionary.ContainsKey(_poolPrefabID))
+            {
+                poolInactive = PoolInactiveDictionary[_poolPrefabID];
+            }
+            else
+            {
+                poolInactive = new List<T>();
+                PoolInactiveDictionary[_poolPrefabID] = poolInactive;
+            }
+
+            return poolInactive;
+        }
+        
+        private List<T> GetPoolActive()
+        {
+            return new List<T>();
+        }
+
+        public PoolContainer(int poolPrefabID, string poolPrefabName)
+        {
+            _poolPrefabID = poolPrefabID;
+            _poolPrefabName = poolPrefabName;
+        }
+
+        public void Deconstruct(out Transform poolParent, out List<T> poolInactive, out List<T> poolActive)
+        {
+            poolParent = GetPoolParent();
+            poolInactive = GetPoolInactive();
+            poolActive = GetPoolActive();
+        }
+    }
+}
