cc128194d91fe99951fde3c1d6496a0e4e54274d
Roman Shulevka <micrarriors@gmail.com>
2025-01-14 18:22:06 +0200

    Update MoveTransform.cs

--- Runtime/Between/MoveTransform.cs
+++ Runtime/Between/MoveTransform.cs
@@ -120,21 +120,7 @@ namespace DataKeeper.Between
                 t = 1 - t;
             }
 
-            try
-            {
-                if (float.IsNaN(t) || float.IsInfinity(t))
-                {
-                    Debug.Log($"t: {t} :: easeType: {easeType}");
-                }
-                else
-                {
-                    target.position = Vector3.Lerp(startPosition, endPosition, t);
-                }
-            }
-            catch (Exception e)
-            {
-                Debug.Log($"Catch > easeType: {easeType}");
-            }
+            target.position = Vector3.Lerp(startPosition, endPosition, t);
         }
 
         private void InvokeOnComplete()
