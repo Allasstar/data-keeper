cba86d0d170fada324c0abed4b9bf308cfaaedfd
Roman Shulevka <micrarriors@gmail.com>
2025-03-07 21:23:43 +0200

    Update SearchableEnumAttributeDrawer.cs

--- Editor/Attributes/SearchableEnumAttributeDrawer.cs
+++ Editor/Attributes/SearchableEnumAttributeDrawer.cs
@@ -1,13 +1,16 @@
 using System;
-using DataKeeper.Attributes;
 using UnityEditor;
+using UnityEditor.IMGUI.Controls;
 using UnityEngine;
+using DataKeeper.Attributes;
 
 namespace DataKeeper.Editor.Attributes
 {
     [CustomPropertyDrawer(typeof(SearchableEnumAttribute))]
     public class SearchableEnumAttributeDrawer : PropertyDrawer
     {
+        private static AdvancedDropdownState dropdownState = new AdvancedDropdownState();
+
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             EditorGUI.BeginProperty(position, label, property);
@@ -20,13 +23,15 @@ namespace DataKeeper.Editor.Attributes
             {
                 if (GUI.Button(position, property.enumDisplayNames[property.enumValueIndex], EditorStyles.popup))
                 {
-                    // Open the dropdown window
-                    SearchableEnumDropdown.Show(position, property.enumDisplayNames, property.enumValueIndex,
+                    // Open the dropdown with state
+                    var dropdown = new SearchableEnumDropdown(dropdownState, property.enumDisplayNames, property.enumValueIndex,
                         selectedIndex =>
                         {
                             property.enumValueIndex = selectedIndex;
                             property.serializedObject.ApplyModifiedProperties();
                         });
+
+                    dropdown.Show(position);
                 }
             }
             else
@@ -37,49 +42,35 @@ namespace DataKeeper.Editor.Attributes
             EditorGUI.EndProperty();
         }
     }
-
-    public class SearchableEnumDropdown : EditorWindow
+    
+    public class SearchableEnumDropdown : AdvancedDropdown
     {
         private string[] enumNames;
         private Action<int> onSelected;
-        private string searchText = "";
-        private Vector2 scrollPos;
 
-        public static void Show(Rect buttonRect, string[] enumNames, int selectedIndex, Action<int> onSelected)
+        public SearchableEnumDropdown(AdvancedDropdownState state, string[] enumNames, int selectedIndex, Action<int> onSelected)
+            : base(state)
         {
-            // Create the window
-            var window = ScriptableObject.CreateInstance<SearchableEnumDropdown>();
-            window.enumNames = enumNames;
-            window.onSelected = onSelected;
-            window.searchText = "";
-
-            // Adjust the position
-            buttonRect.position = GUIUtility.GUIToScreenPoint(buttonRect.position);
-            window.ShowAsDropDown(buttonRect, new Vector2(buttonRect.width, 250));
+            this.enumNames = enumNames;
+            this.onSelected = onSelected;
         }
 
-        private void OnGUI()
+        protected override AdvancedDropdownItem BuildRoot()
         {
-            // Search box
-            EditorGUILayout.BeginHorizontal(EditorStyles.toolbar);
-            searchText = EditorGUILayout.TextField(searchText, GUI.skin.FindStyle("ToolbarSearchTextField"));
-            EditorGUILayout.EndHorizontal();
+            var root = new AdvancedDropdownItem("Select an Option");
 
-            // Display filtered results
-            scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
             for (int i = 0; i < enumNames.Length; i++)
             {
-                if (string.IsNullOrEmpty(searchText) || enumNames[i].ToLower().Contains(searchText.ToLower()))
-                {
-                    if (GUILayout.Button(enumNames[i], EditorStyles.toolbarButton))
-                    {
-                        onSelected?.Invoke(i);
-                        Close();
-                    }
-                }
+                var item = new AdvancedDropdownItem(enumNames[i]) { id = i };
+                root.AddChild(item);
             }
 
-            EditorGUILayout.EndScrollView();
+            return root;
+        }
+
+        protected override void ItemSelected(AdvancedDropdownItem item)
+        {
+            onSelected?.Invoke(item.id);
         }
     }
 }
