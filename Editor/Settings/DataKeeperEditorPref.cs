6664fd44b0b3aa7cf25beb8ed310bf57c95e7593
Roman Shulevka <micrarriors@gmail.com>
2025-07-16 17:56:31 +0300

    Update DataKeeperEditorPref.cs

--- Editor/Settings/DataKeeperEditorPref.cs
+++ Editor/Settings/DataKeeperEditorPref.cs
@@ -6,7 +6,7 @@ namespace DataKeeper.Editor.Settings
     public static class DataKeeperEditorPref
     {
         public static ReactiveEditorPref<bool> EnhanceHierarchyIconPref =
-            new ReactiveEditorPref<bool>(true, "EnhanceHierarchyIcon_Enabled");
+            new ReactiveEditorPref<bool>(false, "EnhanceHierarchyIcon_Enabled");
         
         public static ReactiveEditorPref<PrefabHierarchyIcon> EnhanceHierarchyPrefabIconPref =
             new ReactiveEditorPref<PrefabHierarchyIcon>(PrefabHierarchyIcon.Small, "EnhanceHierarchyPrefabIcon_Enabled");
