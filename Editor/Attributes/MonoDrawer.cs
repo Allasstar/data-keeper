b83aee2a286adb17f858c59f672a02f738eaed58
Roman Shulevka <micrarriors@gmail.com>
2025-07-21 10:57:00 +0300

    0.20.1

--- Editor/Attributes/MonoDrawer.cs
+++ Editor/Attributes/MonoDrawer.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 namespace DataKeeper.Editor.Attributes
 {
-    [CustomEditor(typeof(MonoBehaviour), true), CanEditMultipleObjects]
-    public class MonoDrawer : UnityEditor.Editor
+    // [CustomEditor(typeof(MonoBehaviour), true), CanEditMultipleObjects]
+    // public class MonoDrawer : UnityEditor.Editor
+    // {
+    //     public override void OnInspectorGUI()
+    //     {
+    //         base.OnInspectorGUI();
+    //         PropertyGUI.DrawButtons(target);
+    //         FSMDebugger.DrawFSMDebuger(target);
+    //     }
+    // }
+    
+    [CustomEditor(typeof(Component), true), CanEditMultipleObjects]
+    public class ComponentDrawer : UnityEditor.Editor
     {
         public override void OnInspectorGUI()
         {
