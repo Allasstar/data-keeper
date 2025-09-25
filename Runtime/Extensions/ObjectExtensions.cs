871c58fc466064c01c058eed3bcd8629a95323c0
Roman Shulevka <micrarriors@gmail.com>
2025-09-25 20:06:22 +0300

    v 0.31.0

---
+++ Runtime/Extensions/ObjectExtensions.cs
@@ -0,0 +1,121 @@
+using System;
+
+namespace DataKeeper.Extensions
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Safely casts an object to the specified type T.
+        /// Returns the cast object if successful, otherwise returns the default value for T.
+        /// </summary>
+        /// <typeparam name="T">The target type to cast to</typeparam>
+        /// <param name="obj">The object to cast</param>
+        /// <returns>The cast object or default(T) if cast fails</returns>
+        public static T Cast<T>(this object obj)
+        {
+            if (obj is T result)
+                return result;
+        
+            return default(T);
+        }
+
+        /// <summary>
+        /// Safely casts an object to the specified type T with a custom default value.
+        /// Returns the cast object if successful, otherwise returns the provided default value.
+        /// </summary>
+        /// <typeparam name="T">The target type to cast to</typeparam>
+        /// <param name="obj">The object to cast</param>
+        /// <param name="defaultValue">The default value to return if cast fails</param>
+        /// <returns>The cast object or the provided default value if cast fails</returns>
+        public static T Cast<T>(this object obj, T defaultValue)
+        {
+            if (obj is T result)
+                return result;
+        
+            return defaultValue;
+        }
+
+        /// <summary>
+        /// Attempts to cast an object to the specified type T.
+        /// Returns true if successful and outputs the cast object, otherwise returns false.
+        /// </summary>
+        /// <typeparam name="T">The target type to cast to</typeparam>
+        /// <param name="obj">The object to cast</param>
+        /// <param name="result">The cast result if successful</param>
+        /// <returns>True if cast was successful, false otherwise</returns>
+        public static bool TryCast<T>(this object obj, out T result)
+        {
+            if (obj is T castResult)
+            {
+                result = castResult;
+                return true;
+            }
+        
+            result = default(T);
+            return false;
+        }
+
+        /// <summary>
+        /// Forcefully casts an object to the specified type T.
+        /// Throws InvalidCastException if the cast is not possible.
+        /// </summary>
+        /// <typeparam name="T">The target type to cast to</typeparam>
+        /// <param name="obj">The object to cast</param>
+        /// <returns>The cast object</returns>
+        /// <exception cref="InvalidCastException">Thrown when the cast is not possible</exception>
+        public static T ForceCast<T>(this object obj)
+        {
+            if (obj is T result)
+                return result;
+        
+            throw new InvalidCastException($"Cannot cast object of type '{obj?.GetType().Name ?? "null"}' to type '{typeof(T).Name}'");
+        }
+
+        /// <summary>
+        /// Converts an object to the specified type T using Convert.ChangeType.
+        /// Useful for converting between compatible types (e.g., string to int, int to double).
+        /// </summary>
+        /// <typeparam name="T">The target type to convert to</typeparam>
+        /// <param name="obj">The object to convert</param>
+        /// <returns>The converted object</returns>
+        /// <exception cref="InvalidCastException">Thrown when conversion is not possible</exception>
+        /// <exception cref="FormatException">Thrown when the format is invalid</exception>
+        /// <exception cref="OverflowException">Thrown when the value is too large or small</exception>
+        public static T ConvertTo<T>(this object obj)
+        {
+            if (obj == null)
+                return default(T);
+
+            if (obj is T directCast)
+                return directCast;
+
+            try
+            {
+                return (T)Convert.ChangeType(obj, typeof(T));
+            }
+            catch (Exception ex) when (ex is InvalidCastException || ex is FormatException || ex is OverflowException)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Safely converts an object to the specified type T using Convert.ChangeType.
+        /// Returns the converted object if successful, otherwise returns the default value for T.
+        /// </summary>
+        /// <typeparam name="T">The target type to convert to</typeparam>
+        /// <param name="obj">The object to convert</param>
+        /// <returns>The converted object or default(T) if conversion fails</returns>
+        public static T SafeConvertTo<T>(this object obj)
+        {
+            try
+            {
+                return obj.ConvertTo<T>();
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+    }
+}
---
+++ Runtime/UI Toolkit/Elements/Box.cs
@@ -0,0 +1,133 @@
+using System.Collections.Generic;
+using System.Linq;
+using DataKeeper.UIToolkit;
+using UnityEngine.UIElements;
+
+namespace DataKeeper.UI_Toolkit.Elements
+{
+    [UxmlElement]
+    public partial class Box : VisualElement
+    {
+        private FlexDirection _childDirection = FlexDirection.Column;
+        [UxmlAttribute("childDirection")]
+        public FlexDirection ChildDirection
+        {
+            get => _childDirection;
+            set
+            {
+                _childDirection = value;
+                this.SetFlexDirection(_childDirection);
+                RefreshGap();
+            }
+        }
+
+        private float _childGap = 0f;
+        [UxmlAttribute("childGap")]
+        public float ChildGap
+        {
+            get => _childGap;
+            set
+            {
+                _childGap = value;
+                RefreshGap();
+            }
+        }
+
+        public Box()
+        {
+            this.SetFlexDirection(_childDirection);
+            this.SetOnGeometryChanged(OnGeometryChanged);
+        }
+
+        private void OnGeometryChanged(GeometryChangedEvent evt)
+        {
+            RefreshGap();
+        }
+
+        /// <summary>
+        /// Refreshes the gap by scheduling the application for the next frame
+        /// </summary>
+        public void RefreshGap()
+        {
+            schedule.Execute(ApplyGap).ExecuteLater(0);
+        }
+
+        /// <summary>
+        /// Applies margin between children according to current Direction and Gap.
+        /// </summary>
+        private void ApplyGap()
+        {
+            // Clear all margins first
+            var childList = Children().ToList();
+            foreach (var child in childList)
+            {
+                child.SetMargin(0f);
+            }
+
+            // Early return if no gap or insufficient children
+            if (_childGap <= 0f || childList.Count <= 1)
+                return;
+
+            // Apply gap to all children except the first
+            for (int i = 1; i < childList.Count; i++)
+            {
+                var child = childList[i];
+            
+                switch (_childDirection)
+                {
+                    case FlexDirection.Column:
+                        child.SetMarginTop(_childGap);
+                        break;
+                    case FlexDirection.ColumnReverse:
+                        child.SetMarginBottom(_childGap);
+                        break;
+                    case FlexDirection.Row:
+                        child.SetMarginLeft(_childGap);
+                        break;
+                    case FlexDirection.RowReverse:
+                        child.SetMarginRight(_childGap);
+                        break;
+                }
+            }
+        }
+
+        public new void Add(VisualElement child)
+        {
+            base.Add(child);
+            RefreshGap();
+        }
+        
+        public void Add(List<VisualElement> children)
+        {
+            foreach (var child in children)
+            {
+                base.Add(child);
+            }
+            RefreshGap();
+        }
+
+        public new void Insert(int index, VisualElement child)
+        {
+            base.Insert(index, child);
+            RefreshGap();
+        }
+
+        public new void Remove(VisualElement child)
+        {
+            base.Remove(child);
+            RefreshGap();
+        }
+
+        public new void RemoveAt(int index)
+        {
+            base.RemoveAt(index);
+            RefreshGap();
+        }
+
+        public new void Clear()
+        {
+            base.Clear();
+            RefreshGap();
+        }
+    }
+}
