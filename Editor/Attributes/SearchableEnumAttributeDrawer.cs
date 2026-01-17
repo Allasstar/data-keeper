6959593b9f4bbbb7ca38c4bdb77e28bc379a0f8a
Roman Shulevka <micrarriors@gmail.com>
2026-01-17 17:20:59 +0200

    Add value display option to SearchableEnumAttribute
    Introduces a ShowValue property to SearchableEnumAttribute, allowing enum values to be displayed alongside names in the dropdown. Updates the editor drawer and dropdown logic to support this feature and improve enum handling.

--- Editor/Attributes/SearchableEnumAttributeDrawer.cs
+++ Editor/Attributes/SearchableEnumAttributeDrawer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.IMGUI.Controls;
 using UnityEngine;
@@ -14,55 +15,83 @@ namespace DataKeeper.Editor.Attributes
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             EditorGUI.BeginProperty(position, label, property);
-
-            // Draw label
             position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
 
-            // Ensure the property is an enum
-            if (property.propertyType == SerializedPropertyType.Enum)
-            {
-                if (GUI.Button(position, property.enumDisplayNames[property.enumValueIndex], EditorStyles.popup))
-                {
-                    // Open the dropdown with state
-                    var dropdown = new SearchableEnumDropdown(dropdownState, property.enumDisplayNames,
-                        selectedIndex =>
-                        {
-                            property.enumValueIndex = selectedIndex;
-                            property.serializedObject.ApplyModifiedProperties();
-                        });
-
-                    dropdown.Show(position);
-                }
-            }
-            else
+            if (property.propertyType != SerializedPropertyType.Enum)
             {
                 EditorGUI.LabelField(position, "SearchableEnum only works with enums.");
+                EditorGUI.EndProperty();
+                return;
+            }
+
+            var enumType = fieldInfo.FieldType;
+            
+            // Workaround if enum is inside an array or list
+            if (enumType.IsArray) enumType = enumType.GetElementType();
+
+            var enumValues = Enum.GetValues(enumType).Cast<object>().ToArray();
+            var enumNames = Enum.GetNames(enumType);
+            var underlyingValues = enumValues.Select(v => Convert.ToInt32(v)).ToArray();
+
+            var attr = (SearchableEnumAttribute)attribute;
+
+            int currentValue = property.intValue; // actual underlying int
+            int currentIndex = Array.IndexOf(underlyingValues, currentValue);
+            if (currentIndex < 0) currentIndex = 0; // failsafe for invalid state
+
+            string buttonLabel = SearchableEnumDropdown.GenerateEnumLabel(attr.ShowValue, currentIndex, enumNames, underlyingValues);
+
+            if (GUI.Button(position, buttonLabel, EditorStyles.popup))
+            {
+                var dropdown = new SearchableEnumDropdown(
+                    dropdownState,
+                    enumNames,
+                    underlyingValues,
+                    attr.ShowValue,
+                    selectedValue =>
+                    {
+                        property.intValue = selectedValue;
+                        property.serializedObject.ApplyModifiedProperties();
+                    });
+
+                dropdown.Show(position);
             }
 
             EditorGUI.EndProperty();
         }
     }
-    
+
     public class SearchableEnumDropdown : AdvancedDropdown
     {
-        private string[] enumNames;
-        private Action<int> onSelected;
+        private readonly string[] names;
+        private readonly int[] values;
+        private readonly bool showValue;
+        private readonly Action<int> onSelected;
 
-        public SearchableEnumDropdown(AdvancedDropdownState state, string[] enumNames, Action<int> onSelected)
+        public SearchableEnumDropdown(
+            AdvancedDropdownState state,
+            string[] names,
+            int[] values,
+            bool showValue,
+            Action<int> onSelected)
             : base(state)
         {
-            this.enumNames = enumNames;
+            this.names = names;
+            this.values = values;
+            this.showValue = showValue;
             this.onSelected = onSelected;
-            minimumSize = new Vector2(200, 200);
+            minimumSize = new Vector2(240, 300);
         }
 
         protected override AdvancedDropdownItem BuildRoot()
         {
             var root = new AdvancedDropdownItem("Select an Option");
 
-            for (int i = 0; i < enumNames.Length; i++)
+            for (int i = 0; i < names.Length; i++)
             {
-                var item = new AdvancedDropdownItem(enumNames[i]) { id = i };
+                string display = GenerateEnumLabel(showValue, i, names, values);
+
+                var item = new AdvancedDropdownItem(display) { id = values[i] };
                 root.AddChild(item);
             }
 
@@ -72,6 +101,13 @@ namespace DataKeeper.Editor.Attributes
         protected override void ItemSelected(AdvancedDropdownItem item)
         {
             onSelected?.Invoke(item.id);
+        }
+        
+        public static string GenerateEnumLabel(bool showIndex, int currentIndex, string[] enumNames,  int[] enumValues)
+        {
+            return showIndex
+                ? $"{enumNames[currentIndex]} = {enumValues[currentIndex]}"
+                : enumNames[currentIndex];
         }
     }
 }
--- Runtime/Attributes/SearchableEnumAttribute.cs
+++ Runtime/Attributes/SearchableEnumAttribute.cs
@@ -4,6 +4,11 @@ namespace DataKeeper.Attributes
 {
     public class SearchableEnumAttribute : PropertyAttribute
     {
+        public bool ShowValue { get; }
 
+        public SearchableEnumAttribute(bool showValue = false)
+        {
+            ShowValue = showValue;
+        }
     }
 }
